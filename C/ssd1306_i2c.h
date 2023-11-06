358ec7e6da8e2ced109d3d0619bed83a518c4cf8
Wassim Chegham <github@wassim.dev>
2023-11-06 23:02:59 +0100

    Update ssd1306_i2c.h

--- C/ssd1306_i2c.h
+++ C/ssd1306_i2c.h
@@ -10,7 +10,7 @@ extern int i2cd;
 /**********Select display temperature type**************/
 #define CELSIUS       0
 #define FAHRENHEIT    1
-#define TEMPERATURE_TYPE  FAHRENHEIT
+#define TEMPERATURE_TYPE  CELSIUS
 /**********Select display temperature type**************/
 
 /**********Select display network IP type**************/
@@ -24,7 +24,7 @@ extern int i2cd;
 #define IP_DISPLAY_OPEN     0
 #define IP_DISPLAY_CLOSE    1
 #define IP_SWITCH       IP_DISPLAY_OPEN
-#define CUSTOM_DISPLAY   "UCTRONICS"
+#define CUSTOM_DISPLAY   "NAME"
 /************************Turn off the IP display. Can customize the display****************/
 
 #define BLACK 0
@@ -33,7 +33,7 @@ extern int i2cd;
 
 #define OLED_CMD  0	                 //command
 #define OLED_DATA 1	                //data
-#define SSD1306_I2C_ADDRESS   0x3C	// 011110+SA0+RW - 0x0C    
+#define SSD1306_I2C_ADDRESS   0x3C	// 011110+SA0+RW - 0x0C
 #define SSD1306_SWITCHCAPVCC 0x2
 
 #define SSD1306_LCDWIDTH                  128
@@ -42,7 +42,7 @@ void ssd1306_begin(unsigned int switchvcc, unsigned int i2caddr); //switchvcc sh
 void OLED_ShowString(unsigned char x,unsigned char y, unsigned char *p,unsigned char Char_Size);
 void OLED_ShowChar(unsigned char x,unsigned char y,unsigned char chr,unsigned char Char_Size);
 void OLED_Set_Pos(unsigned char x, unsigned char y);
-void OLED_WR_Byte(unsigned dat,unsigned cmd);  
+void OLED_WR_Byte(unsigned dat,unsigned cmd);
 void Write_IIC_Data(unsigned char IIC_Data);
 void Write_IIC_Command(unsigned char IIC_Command);
 void OLED_DrawBMP(unsigned char x0, unsigned char y0,unsigned char x1, unsigned char y1,unsigned char BMP[][512],unsigned char);
